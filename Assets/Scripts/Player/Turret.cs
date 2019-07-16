cb7f144426ee32aef8e0292ef414f9685d3ac301
Anaitz98 <anaitz98@gmail.com>
2019-07-16 18:10:16 +0200

    turret position

--- Assets/Scripts/Player/Turret.cs
+++ Assets/Scripts/Player/Turret.cs
@@ -10,6 +10,10 @@ public class Turret : MonoBehaviour
     private string upDiagonal = "d";
     private string downDiagonal = "a";
 
+    public Animator Lizard;
+    private bool run;
+    private bool jump;
+
     void Start()
     {
         // facingRight = true;
@@ -18,16 +22,35 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+      run = Lizard.GetBool("Run");
+      jump = Lizard.GetBool("Jump");
+
+      if (run){
+           transform.localPosition = new Vector3(7.4f, 6.3f, 0f);
+       }
+       if (jump){
+           transform.localPosition = new Vector3(2.7f, 0.3f, 0f);
+       }
+       if (!run && !jump){
+           transform.localPosition = new Vector3(5.6f, 9.5f, 0f);
+       }
+
+
         if (Input.GetKey(up)){
           transform.localRotation = Quaternion.Euler(0, 0, 90);
+          transform.localPosition = new Vector3(transform.localPosition.x-3.5f,transform.localPosition.y+2.7f, transform.localPosition.z);
         }else if (Input.GetKey(down)){
           transform.localRotation = Quaternion.Euler(0, 0, -90);
+          transform.localPosition = new Vector3(transform.localPosition.x-3.2f,transform.localPosition.y-3.9f, transform.localPosition.z);
         }else if (Input.GetKey(upDiagonal)){
               transform.localRotation = Quaternion.Euler(0, 0, 45);
+              transform.localPosition = new Vector3(transform.localPosition.x-0.6f,transform.localPosition.y+2.8f, transform.localPosition.z);
         }else if (Input.GetKey(downDiagonal)){
               transform.localRotation = Quaternion.Euler(0, 0, -45);
+              transform.localPosition = new Vector3(transform.localPosition.x-0.3f,transform.localPosition.y-3.4f, transform.localPosition.z);
         }else{
           transform.localRotation = Quaternion.Euler(0, 0, 0);
+
         }
     }
 }
