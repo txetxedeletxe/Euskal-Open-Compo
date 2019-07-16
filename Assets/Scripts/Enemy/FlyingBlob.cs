57f2870c65247d126cd39ec1d42dc5360e57a89d
Anaitz98 <anaitz98@gmail.com>
2019-07-17 00:40:04 +0200

    Flying Rotation Shooter

---
+++ Assets/Scripts/Enemy/FlyingBlob.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBlob : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject masterFirePoint;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+
+        masterFirePoint = this.gameObject.transform.GetChild(0).gameObject;
+      }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      masterFirePoint.transform.Rotate(0f,0f,127f);
+      }
+        // gameObject.transform.rotation = Quaternion.Euler(gameObject.transform.rotation.x,gameObject.transform.rotation.y,gameObject.transform.rotation.z+ 10f);
+
+}
--- Assets/Scripts/Projectiles/FirePoint.cs
+++ Assets/Scripts/Projectiles/FirePoint.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class FirePoint : MonoBehaviour
 {
   public GameObject projectile;
+  public float cadency;
+  private float time;
     // Start is called before the first frame update
-    void Start()
-    {
 
+    void Start(){
+      time =0f;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
+    void Update(){
+      time += Time.deltaTime;
+      if( time >= cadency){
+        time =0;
+        spawnProjectile();
+      }
     }
+
     public void spawnProjectile(){
-      Debug.Log("shot");
       Instantiate(projectile, transform.position, transform.rotation);
     }
 }
