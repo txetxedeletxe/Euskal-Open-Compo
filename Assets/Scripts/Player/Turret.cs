3f5ac87d1ba72f09dddf20d3d1da203069aff049
Anaitz98 <anaitz98@gmail.com>
2019-07-16 15:43:03 +0200

    Turret rotation and firePoint

---
+++ Assets/Scripts/Player/Turret.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // private bool facingRight;
+    private string up = "w";
+    private string down = "s";
+    private string upDiagonal = "d";
+    private string downDiagonal = "a";
+
+    void Start()
+    {
+        // facingRight = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // if (Input.GetKey("left"))
+        // {
+        //     facingRight = false;
+        // }else if (Input.GetKey("right"))
+        // {
+        //     facingRight = true;
+        // }
+        if (Input.GetKey(up)){
+          transform.localRotation = Quaternion.Euler(0, 0, 90);
+        }else if (Input.GetKey(down)){
+          transform.localRotation = Quaternion.Euler(0, 0, -90);
+        }else if (Input.GetKey(upDiagonal)){
+              transform.localRotation = Quaternion.Euler(0, 0, 45);
+        }else if (Input.GetKey(downDiagonal)){
+              transform.localRotation = Quaternion.Euler(0, 0, -45);
+        }else{
+          transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+}
