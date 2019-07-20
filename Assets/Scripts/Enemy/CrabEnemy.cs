1c94c64ed951035464c580d6a1526d27e5542808
k0r0s <ex0k0r0s@gmail.com>
2019-07-20 16:22:20 +0200

    Updated Enemy Scripts and added Life Sprites

--- Assets/Scripts/Enemy/CrabEnemy.cs
+++ Assets/Scripts/Enemy/CrabEnemy.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class CrabEnemy : MonoBehaviour
 {
-  private GameObject firePoint;
+    private GameObject masterFirePoint;
+    private Rigidbody2D rb2d;
+    private MasterFirepoint mFirepoint;
+    public float cadency;
+    private GameObject firePoint;
     // Start is called before the first frame update
     void Start()
     {
-        firePoint = this.gameObject.transform.GetChild(0).gameObject;
-        firePoint.GetComponent<FirePoint>().spawnProjectile();
+        masterFirePoint = this.gameObject.transform.GetChild(0).gameObject;
+        mFirepoint = masterFirePoint.GetComponent<MasterFirepoint>();
+        mFirepoint.enableAttack(cadency);
     }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float prob = Random.Range(0f, 1f);
+        if (prob >= 0.8f)
+        {
+            mFirepoint.enableAttack(cadency);
 
+
+        }
+        else mFirepoint.disableAttack();
     }
+
 }
