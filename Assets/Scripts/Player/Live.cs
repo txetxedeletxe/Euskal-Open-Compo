22c3b0d60634616909c7d11a9fd0aea351054680
k0r0s <ex0k0r0s@gmail.com>
2019-07-22 14:13:30 +0200

    Added Game Over Scene

--- Assets/Scripts/Player/Live.cs
+++ Assets/Scripts/Player/Live.cs
@@ -1,39 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Live : MonoBehaviour
 {
-  public int lives;
-  private bool canBeHit;
-  private float countDown;
-  public GameObject liveBars;
+    public int lives;
+    private bool canBeHit;
+    private float countDown;
+    public GameObject liveBars;
     // Start is called before the first frame update
-    void Start(){
-      canBeHit=true;
-      countDown=1.5f;
-      liveBars.GetComponent<LifeControler>().spawnbars(lives);
+    void Start()
+    {
+        canBeHit = true;
+        countDown = 1.5f;
+        liveBars.GetComponent<LifeControler>().spawnbars(lives);
     }
     // Update is called once per frame
-    void Update(){
-      if(!canBeHit){
-        if(countDown<=0){
-          canBeHit=true;
-        }else{
-          countDown-=Time.deltaTime;
+    void Update()
+    {
+        if (!canBeHit)
+        {
+            if (countDown <= 0)
+            {
+                canBeHit = true;
+            }
+            else
+            {
+                countDown -= Time.deltaTime;
+            }
         }
-      }
-    }
-    private void OnTriggerEnter2D(Collider2D hitInfo) {
 
-      if(canBeHit){
-        if (hitInfo.transform.tag == "bullet"){
-            canBeHit=false;
-            countDown=1.5f;
-            lives -= 1;
-            Debug.Log(lives);
-            liveBars.GetComponent<LifeControler>().removeLife();
-          }
+        if (lives <= 0)
+        {
+            SceneManager.LoadScene(2);
         }
     }
-  }
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+
+        if (canBeHit)
+        {
+            if (hitInfo.transform.tag == "bullet")
+            {
+                canBeHit = false;
+                countDown = 1.5f;
+               
+                lives -= 1;
+                liveBars.GetComponent<LifeControler>().removeLife();
+
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+  public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+
+        Debug.Log("QUIT!");
+
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+
+    }
+
+
+}
