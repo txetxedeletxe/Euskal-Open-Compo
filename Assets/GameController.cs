3686cf558d486a16eb558dd6f1d88e552e9b1698
k0r0s <ex0k0r0s@gmail.com>
2019-07-25 00:47:38 +0200

    Added GameController and Credits

---
+++ Assets/GameController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject spawner;
+    public GameObject jungle;
+    public GameObject ruins;
+    public GameObject desert;
+
+
+    int enemyCount;
+    int type;
+    float speed;
+    float cadency;
+    private bool start;
+    private int enemyTypes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyCount = 1;
+        type = 1;
+        cadency = 1;
+        start = true;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (start)
+        {
+            for(int i=0; i<=enemyCount; i++) {
+                type = Mathf.RoundToInt(Random.Range(0, enemyTypes));
+                spawner.GetComponent<EnemySpawner>().spawnEnemy(cadency, type);
+            }
+
+            gameObject.GetComponent<SpeedController>().updateSpeed();
+            enemyCount++;
+            cadency-=0.02f;
+
+            if (jungle.activeSelf && !ruins.activeSelf && !desert.activeSelf)
+            {
+                jungle.SetActive(false);
+                ruins.SetActive(true);
+               
+            } else if (!jungle.activeSelf && ruins.activeSelf && !desert.activeSelf)
+            {
+                ruins.SetActive(false);
+                desert.SetActive(true);
+            } else if (!jungle.activeSelf && !ruins.activeSelf && desert.activeSelf)
+            {
+                desert.SetActive(false);
+                jungle.SetActive(true);
+            }
+
+
+                start = false;
+        }
+
+        if (enemyCount <= 0)
+        {
+            start = true;
+        }
+    }
+
+
+
+
+    public void updateEnemyCount()
+    {
+        enemyCount -= 1;
+    }
+}
---
+++ Assets/Scripts/ESCAPE.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESCAPE : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+}
